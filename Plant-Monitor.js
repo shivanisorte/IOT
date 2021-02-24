ecee2033830e19974974d1d4d2415d3cde3bfc57
Shivani Sorte <69205415+shivanisorte@users.noreply.github.com>
2021-02-25 00:01:05 +0530

    graphs

--- Plant-Monitor.js
+++ Plant-Monitor.js
@@ -1,1 +1,22 @@
+//normal
+// plotChart("time_stamp","light");
+
+// //linegraph
+// setChartLibrary('google-chart');
+// setChartType('lineGraph');
+// plotChart("time_stamp","light");
+
+// //bargraph
+// setChartLibrary('google-chart');
+// setChartType('barGraph');
+// plotChart("time_stamp","light");
+
+// //scattergraph
+// setChartLibrary('google-chart');
+// setChartType('scatterGraph');
+// plotChart("time_stamp","light");
+
+//areagraph
+setChartLibrary('google-chart');
+setChartType('areaGraph');
 plotChart("time_stamp","light");
