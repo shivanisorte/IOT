c1c66351ad48767dcf9fdbe2a25fb403cfa00941
Shivani Sorte <69205415+shivanisorte@users.noreply.github.com>
2021-02-25 00:25:00 +0530

    diffgraphs

--- Plant-Monitor.js
+++ Plant-Monitor.js
@@ -17,6 +17,27 @@
 // plotChart("time_stamp","light");
 
 //areagraph
-setChartLibrary('google-chart');
-setChartType('areaGraph');
-plotChart("time_stamp","light");
+// setChartLibrary('google-chart');
+// setChartType('areaGraph');
+// plotChart("time_stamp","light");
+
+//Customising charts
+// setChartLibrary('google-chart');
+// setChartTitle('My Plant Monitor');
+// setChartType('areaGraph');
+// setAxisName('Time','Light');
+// setCrosshair(true);
+// plotChart("time_stamp","light");
+
+
+//Guage Graph
+setChartLibrary("google-chart");
+setChartTitle('Gauge Chart');
+setChartType('gauge');
+setAxisName('light');
+setDimensions(400, 400);
+setMaxValue(1023);
+setGreen(250, 500);
+setYellow(501, 725);
+setRed(726, 1023);
+plotChart('light');
